571c4e5a79a68365ce703d82f6efc7f159120e70
Alberto Casero <acasero@agbotraining.com>
2017-12-06 00:17:39 +0100

    Refactorizamos las plantillas utilizando bloques

--- src/ipdb/urls.py
+++ src/ipdb/urls.py
@@ -22,5 +22,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
 
     path('pelis/<int:pk>', movie_detail, name="movie_detail_page"),
-    path('', home)
+    path('', home, name="home_page")
 ]
