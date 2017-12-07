c8bca9449f2e412d24379c75ae931ca2d280c3d9
Alberto Casero <acasero@agbotraining.com>
2017-12-07 22:30:53 +0100

    Pasamos la vista de login basada en funcion a una vista basada en clase

--- src/users/views.py
+++ src/users/views.py
@@ -1,10 +1,15 @@
 from django.contrib import messages
 from django.contrib.auth import authenticate, login as django_login, logout as django_logout
 from django.shortcuts import render, redirect
+from django.views import View
 
 
-def login(request):
-    if request.method == "POST":
+class LoginView(View):
+
+    def get(self, request):
+        return render(request, "login_form.html")
+
+    def post(self, request):
         username = request.POST.get("login_username")
         password = request.POST.get("login_password")
         authenticated_user = authenticate(username=username, password=password)
@@ -13,7 +18,7 @@ def login(request):
             return redirect('home_page')
         else:
             messages.error(request, "Usuario incorrecto o inactivo")
-    return render(request, "login_form.html")
+            return render(request, "login_form.html")
 
 
 def logout(request):
