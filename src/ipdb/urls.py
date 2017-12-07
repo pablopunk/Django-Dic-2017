7f988da2ebbdff454e30eb6fe7eb9327de236caf
Alberto Casero <acasero@agbotraining.com>
2017-12-07 22:21:09 +0100

    Creamos menu de navegacion en funcion de si el usuario esta autenticado o no

--- src/ipdb/urls.py
+++ src/ipdb/urls.py
@@ -17,12 +17,13 @@ from django.contrib import admin
 from django.urls import path
 
 from movies.views import movie_detail, home
-from users.views import login
+from users.views import login, logout
 
 urlpatterns = [
     path('admin/', admin.site.urls),
 
     path('login', login, name="login_page"),
+    path('logout', logout, name="logout_page"),
 
     path('pelis/<int:pk>', movie_detail, name="movie_detail_page"),
     path('', home, name="home_page")
