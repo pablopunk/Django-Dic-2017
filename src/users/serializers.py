a8b3b51c55c8ee6a75acce0ec695a75aafed2b2b
Alberto Casero <acasero@agbotraining.com>
2017-12-12 23:51:49 +0100

    Implementamos el endpoint de actualizacion de usuario mejorando la regla de validacion del nombre de usuario diferencia cuando se crea un usuario o cuando se esta actualizando

--- src/users/serializers.py
+++ src/users/serializers.py
@@ -3,22 +3,31 @@ from rest_framework import serializers
 from rest_framework.exceptions import ValidationError
 
 
-class UserSerializer(serializers.Serializer):
+class UsersListSerializer(serializers.Serializer):
 
     id = serializers.ReadOnlyField()
     first_name = serializers.CharField()
     last_name = serializers.CharField()
+
+
+class UserSerializer(UsersListSerializer):
+
     username = serializers.CharField()
     email = serializers.EmailField()
     password = serializers.CharField()
 
     def validate_username(self, data):
-        if User.objects.filter(username=data).exists():
+        if self.instance is None and User.objects.filter(username=data).exists():
             raise ValidationError("User already exists")
+        if self.instance is not None and self.instance.username != data and User.objects.filter(username=data).exists():
+            raise ValidationError("Wanted username is already in use")
         return data
 
     def create(self, validated_data):
         instance = User()
+        return self.update(instance, validated_data)
+
+    def update(self, instance, validated_data):
         instance.first_name = validated_data.get("first_name")
         instance.last_name = validated_data.get("last_name")
         instance.username = validated_data.get("username")
@@ -26,6 +35,3 @@ class UserSerializer(serializers.Serializer):
         instance.set_password(validated_data.get("password"))
         instance.save()
         return instance
-
-    def update(self, instance, validated_data):
-        pass
