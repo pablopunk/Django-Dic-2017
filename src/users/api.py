7ab9399c8f8d0c63a12f15eb84c6e19567e5cdbf
Alberto Casero <acasero@agbotraining.com>
2017-12-12 22:03:27 +0100

    Implementamos el endpoint de listado de usuarios

--- src/users/api.py
+++ src/users/api.py
@@ -1,5 +1,8 @@
+from django.contrib.auth.models import User
 from rest_framework.response import Response
 from rest_framework.views import APIView
+
+from users.serializers import UserSerializer
 
 
 class HelloWorld(APIView):
@@ -12,3 +15,11 @@ class HelloWorld(APIView):
 
     def put(self, request):
         return Response(request.data)
+
+
+class UsersListAPI(APIView):
+
+    def get(self, request):
+        users = User.objects.all()
+        serializer = UserSerializer(users, many=True)
+        return Response(serializer.data)
