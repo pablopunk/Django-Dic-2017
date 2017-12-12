3dd317ac81eedf4c40c9c3464809e81d5b1b216d
Alberto Casero <acasero@agbotraining.com>
2017-12-13 00:32:42 +0100

    Implemento endpoints de listado y creacion de peliculas utilizando una vista generica de REST Framework

--- src/movies/api.py
+++ src/movies/api.py
@@ -1,13 +1,10 @@
-from rest_framework.response import Response
-from rest_framework.views import APIView
+from rest_framework.generics import ListCreateAPIView
 
 from movies.models import Movie
 from movies.serializers import MovieSerializer
 
 
-class MoviesListAPI(APIView):
+class MoviesListAPI(ListCreateAPIView):
 
-    def get(self, request):
-        movies = Movie.objects.all()
-        serializer = MovieSerializer(movies, many=True)
-        return Response(serializer.data)
+    queryset = Movie.objects.all()
+    serializer_class = MovieSerializer
