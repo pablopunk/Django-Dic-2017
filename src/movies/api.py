15ddc9c97dab1f6133f8853659ea40cacc75349f
Alberto Casero <acasero@agbotraining.com>
2017-12-15 00:17:25 +0100

    Implementamos los endpoints de categorias usando un ModelViewSet y un Router

--- src/movies/api.py
+++ src/movies/api.py
@@ -1,9 +1,10 @@
 from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from rest_framework.viewsets import ModelViewSet
 
-from movies.models import Movie
-from movies.permissions import MoviesPermission
-from movies.serializers import MovieSerializer, MoviesListSerializer
+from movies.models import Movie, Category
+from movies.permissions import MoviesPermission, CategoriesPermission
+from movies.serializers import MovieSerializer, MoviesListSerializer, CategorySerializer
 
 
 class MoviesListAPI(ListCreateAPIView):
@@ -26,3 +27,10 @@ class MovieDetailAPI(RetrieveUpdateDestroyAPIView):
 
     def perform_update(self, serializer):
         serializer.save(user=self.request.user)
+
+
+class CategoryViewSet(ModelViewSet):
+
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+    permission_classes = [CategoriesPermission]
