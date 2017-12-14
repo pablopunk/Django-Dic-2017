0f323be55f013e8f2ecbafca556a8dede89d50c4
Alberto Casero <acasero@agbotraining.com>
2017-12-14 21:16:54 +0100

    Implementamos paginacion de resultados en el listado de usuarios y peliculas

--- src/ipdb/settings.py
+++ src/ipdb/settings.py
@@ -146,3 +146,9 @@ LOGGING = {
 
 # Login URL
 LOGIN_URL = "/login"
+
+# Django REST Framework settings
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 2
+}
--- src/users/api.py
+++ src/users/api.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.models import User
 from rest_framework import status
 from rest_framework.generics import get_object_or_404
+from rest_framework.pagination import PageNumberPagination
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
@@ -24,8 +25,11 @@ class UsersListAPI(APIView):
 
     def get(self, request):
         users = User.objects.all()
-        serializer = UsersListSerializer(users, many=True)
-        return Response(serializer.data)
+        paginator = PageNumberPagination()
+        # paginamos el queryset
+        paginated_users = paginator.paginate_queryset(users, request)
+        serializer = UsersListSerializer(paginated_users, many=True)
+        return paginator.get_paginated_response(serializer.data)
 
     def post(self, request):
         serializer = UserSerializer(data=request.data)
