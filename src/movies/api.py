7e5011e968ac61f6ac6d542d118b805142dcc99e
Alberto Casero <acasero@agbotraining.com>
2017-12-14 22:52:59 +0100

    Implementamos el control de autorizacion en el endpoint de creacion de peliculas

--- src/movies/api.py
+++ src/movies/api.py
@@ -1,4 +1,5 @@
 from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
 
 from movies.models import Movie
 from movies.serializers import MovieSerializer, MoviesListSerializer
@@ -7,12 +8,19 @@ from movies.serializers import MovieSerializer, MoviesListSerializer
 class MoviesListAPI(ListCreateAPIView):
 
     queryset = Movie.objects.all()
+    permission_classes = [IsAuthenticatedOrReadOnly]
 
     def get_serializer_class(self):
         return MoviesListSerializer if self.request.method == "GET" else MovieSerializer
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
 
 
 class MovieDetailAPI(RetrieveUpdateDestroyAPIView):
 
     queryset = Movie.objects.all()
     serializer_class = MovieSerializer
+
+    def perform_update(self, serializer):
+        serializer.save(user=self.request.user)
